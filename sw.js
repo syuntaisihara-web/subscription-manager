429a0a369f88a4187d83389baa88d4c598bbc520
syuntaisihara-web <syunta.isihara@gmail.com>
2026-09-07 01:33:34 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'subscription-manager-v029';
+const CACHE_NAME = 'subscription-manager-v0210';
 const APP_SHELL = [
   './',
   './index.html',
-  './styles.css?v=029',
-  './app.js?v=029',
-  './app_icon.png?v=029',
-  './manifest.json?v=029',
+  './styles.css?v=0210',
+  './app.js?v=0210',
+  './app_icon.png?v=0210',
+  './manifest.json?v=0210',
   './icons/icon-192.png',
   './icons/icon-512.png',
   './icons/apple-touch-icon.png'
